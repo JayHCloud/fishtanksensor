aef2d960c7f20426947fe082a3b395d322c2447c
JayHCloud <144058771+JayHCloud@users.noreply.github.com>
2023-12-04 01:01:29 -0500

    added tasks

--- main/main.c
+++ main/main.c
@@ -17,8 +17,8 @@ void app_main(void)
     tasksGroup = xEventGroupCreate();
 
     xTaskCreatePinnedToCore(tempTask, "TempTask", TASK_STACK_SIZE, NULL, 1, NULL, 1);
-  //  xTaskCreatePinnedToCore(emergencyTask, "EmergencyTask", TASK_STACK_SIZE, NULL, 1, NULL, 1);
-  //  xTaskCreatePinnedToCore(toggleTask, "ToggleTask", TASK_STACK_SIZE, NULL, 2, NULL, 1);
+    xTaskCreatePinnedToCore(emergencyTask, "EmergencyTask", TASK_STACK_SIZE, NULL, 1, NULL, 1);
+    xTaskCreatePinnedToCore(toggleTask, "ToggleTask", TASK_STACK_SIZE, NULL, 1, NULL, 1);
 
     vTaskStartScheduler();
 
@@ -30,3 +30,5 @@ void app_main(void)
 
 
 //TODO Make Generic LED func that takes mqtt? input and changes to colour based on RBG values given
+//TODO Add level sensors to tell me when water needs to be added, on 2nd thought water evaporates pretty consistently and maybe i could just do some math
+//TODO Look into PH sensor
